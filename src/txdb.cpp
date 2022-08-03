5d9b0bd9cb391abbbd112c50667fbb6485878f28
Vasilii <liklo.adm@gmail.com>
2022-08-03 18:16:49 +0300

    fix sync🤷‍

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -286,12 +286,12 @@ void CCoinsViewDBCursor::Next()
 bool CBlockTreeDB::WriteBatchSync(const std::vector<std::pair<int, const CBlockFileInfo*> >& fileInfo, int nLastFile, const std::vector<const CBlockIndex*>& blockinfo) {
     CDBBatch batch(*this);
 
-    LogPrintf("THIS SHIT (%s)\n", blockinfo.ToString());
     for (std::vector<std::pair<int, const CBlockFileInfo*> >::const_iterator it=fileInfo.begin(); it != fileInfo.end(); it++) {
         batch.Write(std::make_pair(DB_BLOCK_FILES, it->first), *it->second);
     }
     batch.Write(DB_LAST_BLOCK, nLastFile);
     for (std::vector<const CBlockIndex*>::const_iterator it=blockinfo.begin(); it != blockinfo.end(); it++) {
+        LogPrintf("THIS SHIT (%s)\n", (*it)->ToString());
         batch.Write(std::make_pair(DB_BLOCK_INDEX, (*it)->GetBlockHash()), CDiskBlockIndex(*it));
     }
     return WriteBatch(batch, true);
