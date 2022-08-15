e01da2ff0b3d32347591280fa0dd8e92424c2e07
Vasilii <liklo.adm@gmail.com>
2022-08-15 22:03:49 +0300

    first try to delete root🤷‍

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -18,7 +18,7 @@
  * of the block.
  */
 
-static const uint32_t nSmartActivationBlock = -1000;
+static const uint32_t nSmartActivationBlock = 1660595140;
 
 class CBlockHeader
 {
@@ -57,7 +57,7 @@ public:
         READWRITE(obj.nHeight);
         READWRITE(obj.nNonce64);
         READWRITE(obj.mix_hash);
-        if (nSmartActivationBlock <= obj.nHeight){
+        if (nSmartActivationBlock <= obj.nTime){
             READWRITE(obj.hashStateRoot);
             READWRITE(obj.hashUTXORoot);
             READWRITE(obj.prevoutStake);
