331e6e932b2359f3efcbbdf18125bcae9e62c146
Vasilii <liklo.adm@gmail.com>
2022-08-16 13:03:46 +0300

    first try to delete root🤷‍

--- src/init.cpp
+++ src/init.cpp
@@ -1648,7 +1648,7 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
 
         do {
             const int64_t load_block_index_start_time = GetTimeMillis();
-            dev::eth::ChainParams cp(chainparams.EVMGenesisInfo());
+
             try {
                 LOCK(cs_main);
                 chainman.Reset();
@@ -1714,6 +1714,7 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
 
 
                 if (active_chain.Height() >= chainparams.GetConsensus().nSmartActivationBlock){
+                    dev::eth::ChainParams cp(chainparams.EVMGenesisInfo());
                     /////////////////////////////////////////////////////////// qtum
                     if((args.IsArgSet("-dgpstorage") && args.IsArgSet("-dgpevm")) || (!args.IsArgSet("-dgpstorage") && args.IsArgSet("-dgpevm")) ||
                       (!args.IsArgSet("-dgpstorage") && !args.IsArgSet("-dgpevm"))){
@@ -1822,8 +1823,10 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
             {
                 LOCK(cs_main);
                 CChain& active_chain = chainman.ActiveChain();
+                std::cout << "active_chain.Height()" << active_chain.Height()<< std::endl;
+                std::cout << "chainparams.GetConsensus().nSmartActivationBlock " << chainparams.GetConsensus().nSmartActivationBlock<< std::endl;
                 if (active_chain.Height() >= chainparams.GetConsensus().nSmartActivationBlock){
-
+                    dev::eth::ChainParams cp(chainparams.EVMGenesisInfo());
                     if(active_chain.Tip() != nullptr){
                         globalState->setRoot(uintToh256(active_chain.Tip()->hashStateRoot));
                         globalState->setRootUTXO(uintToh256(active_chain.Tip()->hashUTXORoot));
