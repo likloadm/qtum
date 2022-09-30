44e9cc99415a27a5c0952fe75eaaa6965eacdb2a
Vasilii <liklo.adm@gmail.com>
2022-09-30 18:02:41 +0300

    add delay.cpp

--- src/validationinterface.cpp
+++ src/validationinterface.cpp
@@ -253,7 +253,7 @@ void CMainSignals::BlockChecked(const CBlock& block, const BlockValidationState&
 void CMainSignals::RelayAlternativeChain(BlockValidationState &state, const std::shared_ptr<const CBlock> pblock, BlockSet* sForkTips) {
 //    LOG_EVENT("%s: block hash=%s state=%s", __func__,
 //              block.GetHash().ToString(), state.ToString());
-    m_internals->Iterate([&](CValidationInterface& callbacks) { callbacks.RelayAlternativeChain(state, pblock, &sForkTips); });
+    m_internals->Iterate([&](CValidationInterface& callbacks) { callbacks.RelayAlternativeChain(state, pblock, sForkTips); });
 }
 
 void CMainSignals::NewPoWValidBlock(const CBlockIndex *pindex, const std::shared_ptr<const CBlock> &block) {
